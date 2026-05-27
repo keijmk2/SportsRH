a64373dea410d0b4b4eb5e5762568da8c55997e6
YuyuHayashi-lang <yarlihurtadob11@gmail.com>
2026-05-27 13:02:29 -0500

    ultima clase de js de frank

--- SportsRH/js/login.js
+++ SportsRH/js/login.js
@@ -1,8 +1,38 @@
 console.log('hola');
-const user = {
+const user = 
+[
+  {
     "email": "admin@admin.com",
     "password": "admin123",
     "cargo": "Administrador"
+  },
+  {
+    "email": "admin@prof.com",
+    "password": "prof123",
+    "cargo": "Profesor"
+  },
+  {
+    "email": "admin@estu.com",
+    "password": "estu123",
+    "cargo": "Estudiante"
+  } 
+]
+
+function InicioSesion(email, password){
+    for (let Index = 0; Index < user.length; Index++ ) {
+        const element = user[Index];
+        if (element.email === email && element.password === password) {
+            alert("Encontrado");
+            sessionStorage.setItem("user",element.user);
+            break
+        }
+        alert("No encontrado")
+
+        console.log("index", Index);
+        console.log("elemento", element, email);
+        console.log("elemento", element, password);
+        console.log("elemento", element, cargo);
+    }
 }
 
 const formulario = document.getElementById("InicioSesion");
